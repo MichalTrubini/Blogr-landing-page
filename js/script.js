07953431172663df16e5f557fc59de9e139747ba
Michal Trubini <mchl.trubini@gmail.com>
2021-11-11 19:31:04 +0100

    js, dt navigation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const menuHeader = document.querySelectorAll('.header__nav-item');
+/* const menuHeader = document.querySelectorAll('.header__nav-item');
 const menuHamburger = document.querySelector('.header__nav-hamburger');
-const menu = document.querySelector('.header__nav-menu');
+
 
 menuHeader.forEach(rollSubMenu);
 menuHamburger.addEventListener('click',toggleMenu);
@@ -19,7 +19,12 @@ function rollSubMenu (item) {
             listRoll.className = 'header__nav-item-list'
         };
     })
-}
+} */
+
+const menu = document.querySelector('.header__nav-menu');
+const menuHamburger = document.querySelector('.header__nav-hamburger');
+
+menuHamburger.addEventListener('click',toggleMenu);
 
 function toggleMenu(){
     if (menu.className === 'header__nav-menu') {
@@ -28,4 +33,36 @@ function toggleMenu(){
     else {
         menu.className = 'header__nav-menu';
     };
+}
+
+const elements = document.querySelectorAll('.header__nav-item');
+
+
+elements.forEach(rollSubMenu);
+
+function rollSubMenu(element) {
+    let subMenuHeader = element.firstElementChild;
+    let subMenu = element.lastElementChild;
+    let subMenus = document.querySelectorAll('.header__nav-item-list');
+    let arrowAnimated = element.querySelector('.header__nav-item-arrow');
+
+    subMenuHeader.addEventListener('click',function(){
+        subMenus.forEach(subms => {
+            if (subMenu !== subms && screen.width < 1100) {
+                subms.classList.remove('header__nav-item-list-display');
+            }
+        });
+
+        if (screen.width < 1100) subMenu.classList.toggle('header__nav-item-list-display');
+
+        if (arrowAnimated.className === 'header__nav-item-arrow-animation-up' && screen.width < 1100) {
+            arrowAnimated.classList.remove('header__nav-item-arrow-animation-up')
+            arrowAnimated.classList.add('header__nav-item-arrow-animation-down');
+        }
+        else {
+            arrowAnimated.className = 'header__nav-item-arrow-animation-up'
+        };
+
+
+    })
 }
